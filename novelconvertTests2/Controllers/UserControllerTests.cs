5db55d2f30dac665fd2201f21ec99a30442d5551
Britoz <lythevinh2222@gmail.com>
2019-05-22 10:55:31 +1000

    travis test

---
+++ novelconvertTests2/Controllers/UserControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using novelconvert.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace novelconvert.Controllers.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void GetAllNovelByUserId()
+        {
+            
+            Assert.Fail();
+        }
+    }
+}
